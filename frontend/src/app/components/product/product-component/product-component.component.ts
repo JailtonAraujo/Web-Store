d4a61c47524ca26a25a98e3f70df828c13430dbc
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-12-07 09:43:03 -0300

    Finalizando pagina inicial

--- frontend/src/app/components/product/product-component/product-component.component.ts
+++ frontend/src/app/components/product/product-component/product-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faHeart as faHeartSolid, faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-product-component',
@@ -9,7 +11,15 @@ export class ProductComponentComponent implements OnInit {
 
   constructor() { }
 
+  iconFavority = faHeartRegular;
+  isFavority:Boolean = false;
+
   ngOnInit(): void {
+  }
+
+  public chengeIconFavority(){
+      this.isFavority = !this.isFavority;
+      this.isFavority ? this.iconFavority = faHeartSolid : this.iconFavority = faHeartRegular
   }
 
 }
--- frontend/src/app/components/product/product.module.ts
+++ frontend/src/app/components/product/product.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProductComponentComponent } from './product-component/product-component.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const components =[
-  ProductComponentComponent
+  ProductComponentComponent,
+  ProductDetailsComponent,
+  ProductListComponent
 ]
 
 @NgModule({
@@ -15,7 +18,8 @@ const components =[
     ProductListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ],
   exports:[
     components
